01ecb4cd57f743f7ed370eeb7e56ef267b5f88a6
titoustaff <hans_leon@hotmail.com>
2019-10-13 01:18:10 +0200

    Add files via upload
    change

--- polyfills.js
+++ polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/xpia057/dev/golfApp/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/marionhans/Documents/Dev/webApp/golfApp/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
